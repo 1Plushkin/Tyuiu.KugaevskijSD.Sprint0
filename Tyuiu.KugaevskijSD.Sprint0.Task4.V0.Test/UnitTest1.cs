86f6a23d9713c02e7f5adeb221dfdcb946be31a6
1Plushkin <Plushkin001@mail.ru>
2025-09-17 00:43:53 +0500

    Task4

---
+++ Tyuiu.KugaevskijSD.Sprint0.Task4.V0.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KugaevskijSD.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, Class1.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, Class1.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(25, Class1.Multiplication(5, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, Class1.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.KugaevskijSD.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KugaevskijSD.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Addition(1, 5));
+            Console.WriteLine(Class1.Subtraction(15, 5));
+            Console.WriteLine(Class1.Multiplication(10, 10));
+            Console.WriteLine(Class1.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
