6e2e65a6191c3076094d4b3f7fbb6ded4abee7f8
1Plushkin <Plushkin001@mail.ru>
2025-09-16 21:01:04 +0500

    Task1

---
+++ Tyuiu.KugaevskijSD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KugaevskijSD.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "1Plushkin";
+            int age = 20;
+            Console.WriteLine("Hi my name is" + name + "am" + age);
+            Console.ReadKey();
+
+        }
+    }
+}
